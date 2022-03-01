0439e418b328212def8e0e89a017605b98c10d11
bryan mancilla <mancillabryan1996@outlook.com>
2022-03-01 14:28:26 -0600

    storage img

--- api/routes/product.js
+++ api/routes/product.js
@@ -2,9 +2,8 @@
 const express = require('express')
 const router = express.Router()
 const {addProduct}= require('../controller/puductController')
+const upload = require('../libs/storage')
 
-router.use(express.json())
-
-router.post('/products',addProduct) 
+router.post('/products',upload.single('image'),addProduct) 
 
 module.exports = router

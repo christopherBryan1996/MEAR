f47df0d2fbacf160af877c9e81c29b14fddd23e9
bryan mancilla <mancillabryan1996@outlook.com>
2022-04-03 00:43:18 -0600

    cors and env and products

--- api/app.js
+++ api/app.js
@@ -2,10 +2,16 @@
 const express = require('express')
 const productRoutes= require('./routes/product')
 const app = express()
+const morgan= require('morgan')
+const cors = require('cors')
+app.use(cors({
+    origin:'*'
+}))
 //para ocupar los json
 app.use(express.json())
 //para ocupar cualquier otra forma de parametros
 app.use(express.urlencoded({extended:false}))
+app.use(morgan('dev'))
 app.use('/api',productRoutes)
 //vamos a poner la ruta de /public
 app.use('/public', express.static(`${__dirname}/storage/imgs`))
--- intro-mern-fronted/src/Products/components/ListProducts.js
+++ intro-mern-fronted/src/Products/components/ListProducts.js
@@ -1,8 +1,23 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 import Loading from "./Loading";
+import {getProducts} from "../services";
 
 const ListProducts= ()=>{
-    const [isLoading, setIsLoading] = useState(false)
+    const [isLoading, setIsLoading] = useState(true)
+    const [products, setProducts]=useState([])
+
+    useEffect(()=>{
+        async function loadProducts(){
+            const response= await getProducts()
+
+            if(response.status === 200){
+                setProducts(response.data.products)
+            }
+        }
+        loadProducts()
+        setIsLoading(false)
+    },[])
+    console.log(products)
     return (
         
         isLoading ? <Loading/>:'Mostrar lista'
